4612a8985ddbc2093ab1e1723ad2ab1fd76505a2
dashingshree6 <dashingshree6@gmail.com>
2022-09-11 22:21:58 +0530

    Create HeaderItem.js

---
+++ client/components/HeaderItem.js
@@ -0,0 +1,25 @@
+import { Fragment } from 'react'
+
+const HeaderItem = ({title, Icon}) => {
+    return (
+        <Fragment>
+            {/* <AiOutlineHome className="h-8 mb-1"/> */}
+            <div 
+            className="flex 
+            flex-col 
+            items-center 
+            cursor-pointer 
+            w-12 
+            group
+            sm:w-20 hover:text-white">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-8 mb-1 text-gray-100">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.745 3.745 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z" />
+                </svg>
+                <p className="opacity-100 tracking-widest group-hover:opacity-100 text-gray-100">{title}</p>
+            </div>
+
+        </Fragment>
+    )
+}
+
+export default HeaderItem
